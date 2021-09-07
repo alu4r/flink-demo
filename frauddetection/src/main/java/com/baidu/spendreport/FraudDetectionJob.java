d5b1f8ea767070203d3a298f76fbd3603a2677f8
liulu40 <liulu40@baidu.com>
2021-09-07 15:58:26 +0800

    Initial commit

---
+++ frauddetection/src/main/java/com/baidu/spendreport/FraudDetectionJob.java
@@ -0,0 +1,32 @@
+package com.baidu.spendreport;
+
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.walkthrough.common.sink.AlertSink;
+import org.apache.flink.walkthrough.common.entity.Alert;
+import org.apache.flink.walkthrough.common.entity.Transaction;
+import org.apache.flink.walkthrough.common.source.TransactionSource;
+
+/**
+ * @author alu4r
+ **/
+public class FraudDetectionJob {
+    public static void main(String[] args) throws Exception {
+        // 用于设置执行环境。任务执行环境用于定义任务的属性、创建数据源以及最终启动任务的执行。
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<Transaction> transactions = env
+                .addSource(new TransactionSource())
+                .name("transactions");
+
+        DataStream<Alert> alerts = transactions
+                .keyBy(Transaction::getAccountId)
+                .process(new FraudDetector())
+                .name("fraud-detector");
+
+        alerts.addSink(new AlertSink())
+                .name("send-alerts");
+
+        env.execute("Fraud Detection");
+    }
+}
